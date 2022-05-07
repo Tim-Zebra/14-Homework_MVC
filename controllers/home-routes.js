6b3c62aaebcd383b84ada11c33878137a149c99a
TZebra <100721290+Tim-Zebra@users.noreply.github.com>
2022-05-06 22:05:53 -0500

    able to display main screen by linking to all-posts.hand

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -5,12 +5,51 @@ const { Post, Comment, User } = require('../models/');
 
 // get all posts for homepage
 router.get('/', async (req, res) => {
-  
+  try {
+    // Get all posts and comments
+    const postData = await Post.findAll({
+      include: [
+        {
+          model: Comment,
+        },
+      ],
+    });
+
+    // Gets data from posts
+    const posts = postData.map((post) => post.get({ plain: true }));
+
+    // Passes posts and session data into mustache
+    res.render('all-posts', { 
+      posts, 
+      logged_in: req.session.logged_in 
+    });
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 // get single post
 router.get('/post/:id', async (req, res) => {
- 
+  try {
+    const postData = await Post.findByPk(req.params.id, {
+      include: [
+        {
+          model: Comment,
+        },
+      ],
+    });
+
+    // Gets data from single post
+    const post = postData.get({ plain: true });
+
+    // Passes post and session status to mustache
+    res.render('post', {
+      ...post,
+      logged_in: req.session.logged_in
+    });
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 router.get('/login', (req, res) => {
